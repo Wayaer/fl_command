cd05383e3c5c3ecf983c3bd9ea20f7f192bb1211
Wayaer <wayaer@foxmail.com>
2022-01-24 17:10:13 +0800

    no message

--- lib/src/terminal/terminal.dart
+++ lib/src/terminal/terminal.dart
@@ -102,7 +102,7 @@ class TerminalStyle {
     this.noneColor = const TerminalColor(Colors.blue, Colors.white),
     this.echoColor =
         const TerminalColor(Colors.deepPurpleAccent, Colors.deepPurpleAccent),
-    this.outputColor = const TerminalColor(Colors.blue, Colors.blue),
+    this.outputColor = const TerminalColor(Colors.blue, Colors.white),
     this.runColor = const TerminalColor(Colors.green, Colors.green),
     this.exceptionColor = const TerminalColor(Colors.red, Colors.red),
   });
