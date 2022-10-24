56cac0ff6b43a2907753f64c7af598d415877d78
Wayaer <wayaer@foxmail.com>
2022-10-24 11:16:50 +0800

    no message

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import desktop_drop
 import flutter_curiosity
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   CuriosityPlugin.register(with: registry.registrar(forPlugin: "CuriosityPlugin"))
 }
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <desktop_drop/desktop_drop_plugin.h>
 #include <flutter_curiosity/curiosity_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  DesktopDropPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DesktopDropPlugin"));
   CuriosityPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("CuriosityPlugin"));
 }
