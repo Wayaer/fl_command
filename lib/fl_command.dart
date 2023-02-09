574bf7daff3b47d55c7299ae14757ee019cb79e3
Wayaer <wayaer@foxmail.com>
2023-02-09 10:51:24 +0800

    no message

--- lib/fl_command.dart
+++ lib/fl_command.dart
@@ -6,7 +6,6 @@ export 'package:process_run/shell.dart';
 export 'src/adb/adb_process.dart';
 export 'src/adb/adb_script.dart';
 export 'src/process/process.dart';
-export 'src/process/process.dart';
 export 'src/process/runtime_environment.dart';
 export 'src/scrcpy/scrcpy_process.dart';
 export 'src/scrcpy/scrcpy_script.dart';
